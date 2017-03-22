9e90196997be579303c58d085dbdb4407cdf5aac
Henrique Martins <henrique.m_s@hotmail.com>
2017-03-22 10:59:25 -0300

    up 3
    DLL load failed: The specified module could not be found.

--- org_files/setup.py
+++ org_files/setup.py
@@ -1,11 +1,11 @@
 import sys, os
 from cx_Freeze import setup, Executable
 
-os.environ['TCL_LIBRARY'] = r'C:\Users\aluno\AppData\Local\Programs\Python\Python35-32\tcl\tcl8.6'
-os.environ['TK_LIBRARY'] = r'C:\Users\aluno\AppData\Local\Programs\Python\Python35-32\tcl\tk8.6'
+os.environ['TCL_LIBRARY'] = r'C:\Users\souza\AppData\Local\Programs\Python\Python36-32\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\souza\AppData\Local\Programs\Python\Python36-32\tcl\tk8.6'
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os","tkinter"], "include_files":[r'C:\Users\aluno\AppData\Local\Programs\Python\Python35-32\tcl\tcl8.6',
-r'C:\Users\aluno\AppData\Local\Programs\Python\Python35-32\tcl\tk8.6']}
+build_exe_options = {"packages": ["os","tkinter"], "include_files":['Made_Folder.py','File_Organizator.py',r'C:\Users\souza\AppData\Local\Programs\Python\Python36-32\tcl\tcl8.6',
+r'C:\Users\souza\AppData\Local\Programs\Python\Python36-32\tcl\tk8.6',r'C:\Users\souza\AppData\Local\Programs\Python\Python36-32\Lib\tkinter']}
 #build_exe_options= {"packages":["os"]}
 
 
@@ -20,4 +20,3 @@ setup(  name = "File Organizator",
         description = "Simple File Organizator made with python 3" ,
         options = {"build_exe": build_exe_options},
         executables = [Executable("File_Organizator.py", base=base)])
-#http://stackoverflow.com/questions/42077990/cx-freeze-dll-load-error-with-tkinter
